a1eb9bcde155e4eb0295d4b470a604c66414134b
root <root@igorvpn.com>
2024-10-29 17:33:45 +0100

    fix

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-    fmt
-    log
-    net/http
+    "fmt"
+    "log"
+    "net/http"
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello, World!")
 }
 
 func main() {
-    http.HandleFunc(/, helloWorldHandler)
+    http.HandleFunc("/", helloWorldHandler)
 
-    port := :8733
-    fmt.Printf(Сервер запущен на порту %sn, port)
+    port := ":8733"
+    fmt.Printf("Сервер запущен на порту %s\n", port)
     if err := http.ListenAndServe(port, nil); err != nil {
-        log.Fatalf(Ошибка запуска сервера: %s, err)
+        log.Fatalf("Ошибка запуска сервера: %s", err)
     }
 }
