3f115b4a7073fbe55aa54ccf0e1437d983f4d0b0
root <root@igorvpn.com>
2024-10-29 17:31:49 +0100

    change port to 8733

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
     http.HandleFunc(/, helloWorldHandler)
 
-    port := :8080
+    port := :8733
     fmt.Printf(Сервер запущен на порту %sn, port)
     if err := http.ListenAndServe(port, nil); err != nil {
         log.Fatalf(Ошибка запуска сервера: %s, err)
